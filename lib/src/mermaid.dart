d3e88aec33a42c0e18fc60e03b2c7c7fe9e87c64
dazemc <daazedjmcfarland@gmail.com>
2025-11-24 19:48:04 -0800

    move render logic out of content, output depending on release/debug

--- lib/src/mermaid.dart
+++ lib/src/mermaid.dart
@@ -4,40 +4,7 @@ import 'package:jaspr/server.dart';
 import 'package:jaspr_content/jaspr_content.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:syntax_highlight_lite/syntax_highlight_lite.dart';
-import 'package:uuid/uuid.dart';
-
-class MermaidRender {
-  //TODO: hash/cache svg
-  MermaidRender._();
-  static final MermaidRender _instance = MermaidRender._();
-  static const _command = 'mmdc';
-
-  factory MermaidRender() {
-    return _instance;
-  }
-
-  void subProcess(String mermaidString, String uuid) async {
-    String svgDir = './images/mermaid/';
-    if (!await File("index.html").exists()) {
-      svgDir = './web/images/mermaid/';
-    }
-    final dir = Directory(svgDir);
-    if (!await dir.exists()) {
-      dir.create(recursive: true);
-    }
-    final List<String> arguments = <String>[
-      '--input',
-      '-',
-      '-o',
-      '$svgDir$uuid.svg',
-    ];
-    final proc = await Process.start(_command, arguments);
-    mermaidString
-        .split('\n')
-        .forEach((String line) => proc.stdin.writeln(line));
-    proc.stdin.close();
-  }
-}
+import 'mermaid_render.dart';
 
 class MermaidBlock extends CustomComponent {
   MermaidBlock({
@@ -126,7 +93,7 @@ class MermaidStaticComponent extends StatelessComponent {
   @override
   Component build(BuildContext context) {
     final MermaidRender mermaidRender = .new();
-    final String uuid = _fastHash(mermaidString).toString();
+    final String uuid = fastHash(mermaidString).toString();
     print(uuid);
     print("MERMAID: Rendering svg elements");
     final String location = './images/mermaid/$uuid.svg';
@@ -138,26 +105,6 @@ class MermaidStaticComponent extends StatelessComponent {
       mermaidRender.subProcess(mermaidString, uuid);
     }
     print(location);
-
-    // return pre(classes: ('mermaid'), [text(component.definition.toString())]);
-    // TODO: make a better svg lib
     return img(src: location);
-  }
-
-  int _fastHash(String string) {
-    //https://pub.dev/documentation/logs_vault/latest/schemes_logs_vault/fastHash.html
-    var hash = 0xcbf29ce484222325;
-
-    var i = 0;
-    while (i < string.length) {
-      final codeUnit = string.codeUnitAt(i++);
-      hash ^= codeUnit >> 8;
-      hash *= 0x100000001b3;
-      hash ^= codeUnit & 0xFF;
-      hash *= 0x100000001b3;
-    }
-
-    // fit signed bit, keep lower 31 bits
-    return hash & 0x7FFFFFFF;
   }
 }
---
+++ lib/src/mermaid_render.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+import 'package:jaspr/jaspr.dart' as j;
+
+class MermaidRender {
+  MermaidRender._();
+  static final MermaidRender _instance = MermaidRender._();
+  static const _command = 'mmdc';
+  static const buildDir = 'build/jaspr/';
+
+  factory MermaidRender() {
+    return _instance;
+  }
+
+  void subProcess(String mermaidString, String uuid) async {
+    late String svgDir;
+    if (j.kDebugMode) {
+      svgDir = './web/images/mermaid/';
+    } else {
+      Directory(buildDir).create(recursive: true);
+      svgDir = '$buildDir/images/mermaid/';
+    }
+    final dir = Directory(svgDir);
+    if (!await dir.exists()) {
+      dir.create(recursive: true);
+    }
+    final List<String> arguments = <String>[
+      '--input',
+      '-',
+      '-o',
+      '$svgDir$uuid.svg',
+    ];
+    final proc = await Process.start(_command, arguments);
+    mermaidString
+        .split('\n')
+        .forEach((String line) => proc.stdin.writeln(line));
+    proc.stdin.close();
+  }
+}
+
+int fastHash(String string) {
+  //https://pub.dev/documentation/logs_vault/latest/schemes_logs_vault/fastHash.html
+  var hash = 0xcbf29ce484222325;
+
+  var i = 0;
+  while (i < string.length) {
+    final codeUnit = string.codeUnitAt(i++);
+    hash ^= codeUnit >> 8;
+    hash *= 0x100000001b3;
+    hash ^= codeUnit & 0xFF;
+    hash *= 0x100000001b3;
+  }
+
+  // fit signed bit, keep lower 31 bits
+  return hash & 0x7FFFFFFF;
+}
